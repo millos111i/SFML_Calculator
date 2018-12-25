7a90a8c48f22346a3e49168209982c198a338595
Kamil Wlazło <kwlazlo10@gmail.com>
2018-12-25 23:17:01 +0100

    Button set methods

--- Calculator/Button.cpp
+++ Calculator/Button.cpp
@@ -4,9 +4,28 @@
 
 Button::Button()
 {
+	
 }
 
 
 Button::~Button()
 {
+}
+
+void Button::setText(const std::string & newText)
+{
+	text.setString(newText);
+	centerText();
+}
+
+void Button::setTextSize(unsigned int size)
+{
+	 text.setCharacterSize(size); 
+	 centerText();
+}
+
+void Button::centerText()
+{
+	text.setOrigin(text.getLocalBounds().width / 2.0, text.getLocalBounds().height / 2.0);
+	text.setPosition(rectangle.getSize().x / 2, rectangle.getSize().y / 2);
 }
--- Calculator/Button.h
+++ Calculator/Button.h
@@ -1,11 +1,25 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+#include"SFML/System/String.hpp"
 class Button
 {
 public:
 	Button();
 	~Button();
 private:
-	
+	sf::RectangleShape rectangle;
+	sf::Color backgroundColor;
+	sf::Color backgroundHoverColor;
+	sf::Color backgroundClickedColor;
+	sf::Text text;
+	sf::Color textColor;
+	bool isPresed;
+	void setButtonColor(sf::Color color) { backgroundColor = color; };
+	void setBackgroundHoverColor(sf::Color color) { backgroundHoverColor = color; }
+	void setBackgroundClickedColor(sf::Color color) { backgroundClickedColor = color; }
+	void setText(const std::string& text);
+	void setTextColor(sf::Color color) { text.setColor(color); }
+	void setTextSize(unsigned int size);
+	void centerText();
 };
 
